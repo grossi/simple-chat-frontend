6ffe5f4aa1641b0140db115180fbb2a8e3373a5b
Gabriel Rossi <gabriel.rossi-ext@viavarejo.com.br>
2021-02-16 11:28:32 -0300

    feat: Sends message on Enter, shift + Enter creates a line break. Changes formatting

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { gql, useMutation } from '@apollo/client';
-import { TextField, Button } from '@material-ui/core';
-import Alert from '@material-ui/lab/Alert';
-import { withStyles } from '@material-ui/core/styles';
-import styles from './LoginStyle.js';
-import bcrypt from 'bcryptjs';
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { TextField, Button } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./LoginStyle.js";
+import bcrypt from "bcryptjs";
 
 const AUTH_USER = gql`
   mutation AUTH_USER($name: String!, $password: String!) {
@@ -29,35 +29,39 @@ const PASSWORD_SALT = gql`
 function Login(props) {
   const { classes } = props;
   const [isRegistering, setIsRegistering] = useState(false);
-  const [userNameInput, setUserNameInput] = useState('');
-  const [passwordInput, setPasswordInput] = useState('');
-  const [passwordConfirmationInput, setPasswordConfirmationInput] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [addUser] = useMutation(ADD_USER, { errorPolicy: 'all' });
-  const [authUser] = useMutation(AUTH_USER, { errorPolicy: 'all' });
-  const [getSalt] = useMutation(PASSWORD_SALT, { errorPolicy: 'all' });
+  const [userNameInput, setUserNameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const [passwordConfirmationInput, setPasswordConfirmationInput] = useState(
+    ""
+  );
+  const [errorMessage, setErrorMessage] = useState("");
+  const [addUser] = useMutation(ADD_USER, { errorPolicy: "all" });
+  const [authUser] = useMutation(AUTH_USER, { errorPolicy: "all" });
+  const [getSalt] = useMutation(PASSWORD_SALT, { errorPolicy: "all" });
 
   const switchView = () => {
-    setUserNameInput('');
-    setPasswordInput('');
-    setPasswordConfirmationInput('');
-    setErrorMessage('');
+    setUserNameInput("");
+    setPasswordInput("");
+    setPasswordConfirmationInput("");
+    setErrorMessage("");
     setIsRegistering(!isRegistering);
-  }
+  };
 
-  const hashPassword = async password => {
+  const hashPassword = async (password) => {
     let salt = await bcrypt.genSalt(10);
     let hash = await bcrypt.hash(password, salt);
-    return { salt, password:hash };
-  }
+    return { salt, password: hash };
+  };
 
-  const login = async event => {
+  const login = async (event) => {
     event.preventDefault();
-    setErrorMessage('');
+    setErrorMessage("");
 
-    const getSaltResponse = await getSalt({ variables: { name: userNameInput }});
+    const getSaltResponse = await getSalt({
+      variables: { name: userNameInput },
+    });
 
-    if( getSaltResponse.errors ) {
+    if (getSaltResponse.errors) {
       setErrorMessage(getSaltResponse.errors[0].message);
       return false;
     }
@@ -66,45 +70,49 @@ function Login(props) {
 
     const password = await bcrypt.hash(passwordInput, salt);
 
-    const authUserResponse = await authUser({ variables: { name: userNameInput, password } });
+    const authUserResponse = await authUser({
+      variables: { name: userNameInput, password },
+    });
 
-    if( authUserResponse.errors ) {
+    if (authUserResponse.errors) {
       setErrorMessage(authUserResponse.errors[0].message);
       return false;
     }
 
     const token = authUserResponse.data.authUser;
 
-    localStorage.setItem('token', token);
-    window.location.assign('/');
-  }
+    localStorage.setItem("token", token);
+    window.location.assign("/");
+  };
 
-  const register = async event => {
+  const register = async (event) => {
     event.preventDefault();
-    setErrorMessage('');
+    setErrorMessage("");
 
-    if( passwordInput === passwordConfirmationInput ) {
+    if (passwordInput === passwordConfirmationInput) {
       const { salt, password } = await hashPassword(passwordInput);
 
-      const addUserResponse = await addUser({ variables: { name: userNameInput, password, passwordSalt: salt } });
+      const addUserResponse = await addUser({
+        variables: { name: userNameInput, password, passwordSalt: salt },
+      });
 
-      if( addUserResponse.errors ) {
+      if (addUserResponse.errors) {
         setErrorMessage(addUserResponse.errors[0].message);
         return false;
       }
 
       switchView();
     } else {
-      setErrorMessage('Passwords don\'t match');
+      setErrorMessage("Passwords don't match");
       return false;
     }
-  }
+  };
 
   return (
-    <div className={classes.backDrop}> 
+    <div className={classes.backDrop}>
       <div className={classes.root}>
-        { !isRegistering ?
-          (<form onSubmit={login}>
+        {!isRegistering ? (
+          <form onSubmit={login}>
             <h2> Login </h2>
             {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
             <fieldset className={classes.loginForm}>
@@ -118,9 +126,9 @@ function Login(props) {
                     required
                     autoComplete="username"
                     margin="dense"
-                    variant="outlined" 
+                    variant="outlined"
                     value={userNameInput}
-                    onChange={e=>setUserNameInput(e.target.value)}
+                    onChange={(e) => setUserNameInput(e.target.value)}
                   />
                 </div>
                 <div className={classes.inputField}>
@@ -132,9 +140,9 @@ function Login(props) {
                     required
                     autoComplete="password"
                     margin="dense"
-                    variant="outlined" 
+                    variant="outlined"
                     value={passwordInput}
-                    onChange={e=>setPasswordInput(e.target.value)}
+                    onChange={(e) => setPasswordInput(e.target.value)}
                   />
                 </div>
               </div>
@@ -156,76 +164,78 @@ function Login(props) {
                 Register
               </Button>
             </fieldset>
-          </form>)
-          :
-          (<form onSubmit={register}>
-          <h2> Register </h2>
-          {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
-          <fieldset className={classes.loginForm}>
-            <div className={classes.inputRow}>
-              <div className={classes.inputField}>
-                <TextField
-                  className={classes.input}
-                  label="Username"
-                  id="username"
-                  type="text"
-                  required
-                  autoComplete="username"
-                  margin="dense"
-                  variant="outlined" 
-                  value={userNameInput}
-                  onChange={e=>setUserNameInput(e.target.value)}
-                />
+          </form>
+        ) : (
+          <form onSubmit={register}>
+            <h2> Register </h2>
+            {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+            <fieldset className={classes.loginForm}>
+              <div className={classes.inputRow}>
+                <div className={classes.inputField}>
+                  <TextField
+                    className={classes.input}
+                    label="Username"
+                    id="username"
+                    type="text"
+                    required
+                    autoComplete="username"
+                    margin="dense"
+                    variant="outlined"
+                    value={userNameInput}
+                    onChange={(e) => setUserNameInput(e.target.value)}
+                  />
+                </div>
+                <div className={classes.inputField}>
+                  <TextField
+                    className={classes.input}
+                    label="Password"
+                    id="password"
+                    type="password"
+                    required
+                    autoComplete="password"
+                    margin="dense"
+                    variant="outlined"
+                    value={passwordInput}
+                    onChange={(e) => setPasswordInput(e.target.value)}
+                  />
+                </div>
+                <div className={classes.inputField}>
+                  <TextField
+                    className={classes.input}
+                    label="Confirm Password"
+                    id="confirmpassword"
+                    type="password"
+                    required
+                    autoComplete="passwordconfirmation"
+                    margin="dense"
+                    variant="outlined"
+                    value={passwordConfirmationInput}
+                    onChange={(e) =>
+                      setPasswordConfirmationInput(e.target.value)
+                    }
+                  />
+                </div>
               </div>
-              <div className={classes.inputField}>
-                <TextField
-                  className={classes.input}
-                  label="Password"
-                  id="password"
-                  type="password"
-                  required
-                  autoComplete="password"
-                  margin="dense"
-                  variant="outlined" 
-                  value={passwordInput}
-                  onChange={e=>setPasswordInput(e.target.value)}
-                />
-              </div>
-              <div className={classes.inputField}>
-                <TextField
-                  className={classes.input}
-                  label="Confirm Password"
-                  id="confirmpassword"
-                  type="password"
-                  required
-                  autoComplete="passwordconfirmation"
-                  margin="dense"
-                  variant="outlined" 
-                  value={passwordConfirmationInput}
-                  onChange={e=>setPasswordConfirmationInput(e.target.value)}
-                />
-              </div>
-            </div>
-            <Button
-              id="submit-button"
-              type="submit"
-              size="large"
-              className={classes.submitButton}
-            >
-              Register
-            </Button>
-            <Button
-              id="start-register-button"
-              type="button"
-              size="small"
-              className={classes.registerButton}
-              onClick={switchView}
-            >
-              Login
-            </Button>
-          </fieldset>
-        </form>)
-        }
+              <Button
+                id="submit-button"
+                type="submit"
+                size="large"
+                className={classes.submitButton}
+              >
+                Register
+              </Button>
+              <Button
+                id="start-register-button"
+                type="button"
+                size="small"
+                className={classes.registerButton}
+                onClick={switchView}
+              >
+                Login
+              </Button>
+            </fieldset>
+          </form>
+        )}
       </div>
     </div>
   );
